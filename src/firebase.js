082b37c2a8a063aad23094c8da7f68a80ade61cf
rodeok <nationx22@gmail.com>
2022-06-08 12:59:15 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const app = firebase.initializeApp({
+  
+})
+
+const firestore = app.firestore()
+export const database = {
+  folders: firestore.collection("folders"),
+  files: firestore.collection("files"),
+  formatDoc: doc => {
+    return { id: doc.id, ...doc.data() }
+  },
+  getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
+}
+export const storage = app.storage()
+export const auth = app.auth()
+export default app
